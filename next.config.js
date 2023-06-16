cbe3ba77e47fd998d168f5020b8926f5ebc64432
Alex Davies <MorickClive@Gmail.com>
2023-06-16 04:20:01 +0100

    AD: removed redirects

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/home',
-                permanent: true,
-            },
-            {
-                source: '/blog',
-                destination: '/work-in-progress',
-                permanent: true,
-            }
-        ]
-    }
+    output: 'export'
 }
 
 module.exports = nextConfig
