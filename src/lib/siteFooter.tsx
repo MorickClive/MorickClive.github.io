378119bb1fe22e2dcc87eae92f2b85c75be99058
Alex Davies <MorickClive@Gmail.com>
2023-07-13 17:55:09 +0100

    AD: adjusted css for blog page

--- src/lib/siteFooter.tsx
+++ src/lib/siteFooter.tsx
@@ -3,8 +3,8 @@ import { GrLinkedin, GrGithub } from "react-icons/gr";
 
 export default async function SiteFooter() {
     return <footer>
-        <Link target="_blank" rel="noopener noreferrer" href="https://github.com/MorickClive" ><GrGithub /></Link>
-        <Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alan-davies-b9957b100/" ><GrLinkedin /></Link>
+        <Link title="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/MorickClive" ><GrGithub /></Link>
+        <Link title="LinkedIn" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alan-davies-b9957b100/" ><GrLinkedin /></Link>
         <h4>|</h4>
 
         <em>Alan Davies. © {new Date().getFullYear()}</em>
